d5f2df65bdaf364aafb2bbcf88514219a285393e
Batmetalstn <stijn.sijstermans@gmail.com>
2026-08-22 02:20:18 +0200

    Update countdown.js

--- src/commands/gmm/countdown.js
+++ src/commands/gmm/countdown.js
@@ -1,7 +1,6 @@
 client.on('messageCreate', message => {
     console.log('Bericht ontvangen:', message.content);
-});
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+});import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 
 export default {
     data: new SlashCommandBuilder()
